76b329dadea4a64f99131cf74b69a77cb284c953
xiuw <sylvi.xw@gmail.com>
2019-08-23 22:14:18 -0400

    dockerfile

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,56 +1,74 @@
-const handleEntry = (req,res,db) => {
-	const{category,title,description,email,phone,address,city,state,budget,account_id} = req.body;
-	if(!category ||!title|| !description||!phone ||!city || !state || !account_id){
-		return res.status(400).json('Error')
-	}
-	db.transaction(trx=>{
-		trx.insert({
-		category: category,
-		title: title,
-		description: description,
-		email:email,
-		phone: phone,
-		address: address,
-		city: city,
-		state:state,
-		budget:budget,
-		post_date: new Date(),
-		account_id:account_id
-	})
-	.into('post')
-	.returning('account_id')
-	.then(id => {
-	  return trx('account')
-		.returning("entries")
-		.where("id","=",id[0])
-		.increment('entries',1)
-		.returning('id')
-		.then(id=> res.json(id[0]));
-	})
-	.then(trx.commit)
-	.catch(trx.rollback)
-	})
-	.catch(err=> res.status(400).json("unable to post"))	
+const handleEntry = (req, res, db) => {
+  const {
+    category,
+    title,
+    description,
+    email,
+    phone,
+    address,
+    city,
+    state,
+    budget,
+    account_id
+  } = req.body;
+  if (
+    !category ||
+    !title ||
+    !description ||
+    !phone ||
+    !city ||
+    !state ||
+    !account_id
+  ) {
+    return res.status(400).json("Error");
+  }
+  db.transaction(trx => {
+    trx
+      .insert({
+        category: category,
+        title: title,
+        description: description,
+        email: email,
+        phone: phone,
+        address: address,
+        city: city,
+        state: state,
+        budget: budget,
+        post_date: new Date(),
+        account_id: account_id
+      })
+      .into("post")
+      .returning("account_id")
+      .then(id => {
+        return trx("account")
+          .returning("entries")
+          .where("id", "=", id[0])
+          .increment("entries", 1)
+          .returning("id")
+          .then(id => res.json(id[0]));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json("unable to post"));
+};
 
-}
+const handleSelectPost = (req, res, db) => {
+  const { id } = req.params;
+  console.log(id);
+  db.select("*")
+    .from("post")
+    .where({ id })
+    .then(post => {
+      if (post.length) {
+        res.json(post[0]);
+      } else {
+        res.status(400).json("Post Not Found");
+      }
+    })
+    .catch(err => res.status(400).json("Error getting post"));
+};
 
-
-const handleSelectPost=(req,res,db)=>{
-	const{id} = req.params;
-	console.log(id);
-	db.select('*').from('post').where({id})
-	.then(post=>{
-		if(post.length){
-			res.json(post[0]);
-		}else{
-			res.status(400).json('Post Not Found')
-		}	
-	})
-	.catch(err=> res.status(400).json('Error getting post'))
-}
-
-
-module.exports={
-	handleEntry: handleEntry,
-	handleSelectPost:handleSelectPost
-}
+module.exports = {
+  handleEntry: handleEntry,
+  handleSelectPost: handleSelectPost
+};
--- server.js
+++ server.js
@@ -1,59 +1,71 @@
-const express = require('express');
-const knex = require('knex');
-const bodyParser = require('body-parser');
-const cors= require('cors');
-const bcrypt = require('bcrypt-nodejs');
+const express = require("express");
+const knex = require("knex");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcrypt-nodejs");
+const morgan = require("morgan");
 
-const register = require('./controllers/register');
-const entry = require('./controllers/entry');
-const login = require('./controllers/login');
-const profile = require('./controllers/profile');
-const getentry = require('./controllers/profile');
-const selectPost = require('./controllers/entry');
+const register = require("./controllers/register");
+const entry = require("./controllers/entry");
+const login = require("./controllers/login");
+const profile = require("./controllers/profile");
+const getentry = require("./controllers/profile");
+const selectPost = require("./controllers/entry");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan("combined"));
 
 const db = knex({
-  client: 'pg',
-  // connection: {
-  //   host : '127.0.0.1',
-  //   user : 'Xiuneh',
-  //   password : '',
-  //   database : 'db'
-  // }
+  client: "pg",
   connection: {
-    connectionString:process.env.DATABASE_URL,
-    ssl:true,
+    host: "127.0.0.1",
+    user: "",
+    password: "",
+    database: "db"
   }
+  // connection: {
+  //   connectionString:process.env.DATABASE_URL,
+  //   ssl:true,
+  // }
 });
 
 // db.select('*').from('post').then(data => {
 // 	console.log(data);
 // });
-app.get('/',(req,res)=>{
-  res.send('Server is running');
-})
-
-app.get('/getall', (req,res) =>{db.select('*').from('post')
-	.then(data=> { 
-		let newArr = data.reverse(); 
-		res.json(newArr);
-	})
-});
-app.post('/entry',(req,res) => {entry.handleEntry(req,res,db)});
-app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt)});
-app.post('/login',(req,res) => {login.handleLogin(req,res,db,bcrypt)});
-app.get('/profile/:id',(req,res) => {profile.handleProfile(req,res,db)});
-app.get('/getentry', (req,res)=>{getentry.handleGetEntry(req,res,db)});
-app.get('/post/:id',(req,res)=> {selectPost.handleSelectPost(req,res,db)})
-
-
-app.listen(process.env.PORT || 3000,()=>{
-	console.log(`Running on Port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
+app.get("/getall", (req, res) => {
+  db.select("*")
+    .from("post")
+    .then(data => {
+      let newArr = data.reverse();
+      res.json(newArr);
+    });
+});
+app.post("/entry", (req, res) => {
+  entry.handleEntry(req, res, db);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.post("/login", (req, res) => {
+  login.handleLogin(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+app.get("/getentry", (req, res) => {
+  getentry.handleGetEntry(req, res, db);
+});
+app.get("/post/:id", (req, res) => {
+  selectPost.handleSelectPost(req, res, db);
+});
 
-
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Running on Port ${process.env.PORT}`);
+  // console.log("app running on port 3000");
+});
